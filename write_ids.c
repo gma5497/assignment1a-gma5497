069a20ff731cc050dc3a4e7f5dfe2c2ce7d9d5a0
gma5497 <gma5497@psu.edu>
2024-09-13 15:11:51 -0400

    Added Github and student ID

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "gma5497";
+    char STUDENT_ID[] = "985211352";
 
     return 0 ;
 }
